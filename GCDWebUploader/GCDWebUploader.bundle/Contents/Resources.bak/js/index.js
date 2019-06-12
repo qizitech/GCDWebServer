47969597ad58454446df7490b078e8a99eacfce9
vkill <vkill.net@gmail.com>
2019-06-12 18:14:51 +0800

    Move bundle to pod

--- GCDWebUploader/GCDWebUploader.bundle/Contents/Resources.bak/js/index.js
+++ GCDWebUploader/GCDWebUploader.bundle/Contents/Resources.bak/js/index.js
@@ -1,7 +1,7 @@
 /*
  Copyright (c) 2012-2019, Pierre-Olivier Latour
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  * The name of Pierre-Olivier Latour may not be used to endorse
  or promote products derived from this software without specific
  prior written permission.
-
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -49,21 +49,13 @@ function _showError(message, textStatus, errorThrown) {
   }));
 }
 
-function _showCustomizedError(message) {
-  $('#alerts').prepend(tmpl('template-alert', {
-    level: 'danger',
-    title: 'ERROR: ',
-    description: message
-  }))
-}
-
 function _disableReloads() {
   _reloadingDisabled += 1;
 }
 
 function _enableReloads() {
   _reloadingDisabled -= 1;
-
+  
   if (_pendingReloads.length > 0) {
     _reload(_pendingReloads.shift());
   }
@@ -76,7 +68,7 @@ function _reload(path) {
     }
     return;
   }
-
+  
   _disableReloads();
   $.ajax({
     url: 'list',
@@ -87,7 +79,7 @@ function _reload(path) {
     _showError("Failed retrieving contents of \"" + path + "\"", textStatus, errorThrown);
   }).done(function(data, textStatus, jqXHR) {
     var scrollPosition = $(document).scrollTop();
-
+    
     if (path != _path) {
       $("#path").empty();
       if (path == "/") {
@@ -107,13 +99,13 @@ function _reload(path) {
       }
       _path = path;
     }
-
+    
     $("#listing").empty();
     for (var i = 0, file; file = data[i]; ++i) {
       $(tmpl("template-listing", file)).data(file).appendTo("#listing");
     }
-
-    $(".edit").editable(function(value, settings) {
+    
+    $(".edit").editable(function(value, settings) { 
       var name = $(this).parent().parent().data("name");
       if (value != name) {
         var path = $(this).parent().parent().data("path");
@@ -141,19 +133,19 @@ function _reload(path) {
       },
       tooltip: 'Click to rename...'
     });
-
+    
     $(".button-download").click(function(event) {
       var path = $(this).parent().parent().data("path");
       setTimeout(function() {
         window.location = "download?path=" + encodeURIComponent(path);
       }, 0);
     });
-
+    
     $(".button-open").click(function(event) {
       var path = $(this).parent().parent().data("path");
       _reload(path);
     });
-
+    
     $(".button-move").click(function(event) {
       var path = $(this).parent().parent().data("path");
       if (path[path.length - 1] == "/") {
@@ -163,7 +155,7 @@ function _reload(path) {
       $("#move-input").val(path);
       $("#move-modal").modal("show");
     });
-
+    
     $(".button-delete").click(function(event) {
       var path = $(this).parent().parent().data("path");
       $.ajax({
@@ -177,7 +169,7 @@ function _reload(path) {
         _reload(_path);
       });
     });
-
+    
     $(document).scrollTop(scrollPosition);
   }).always(function() {
     _enableReloads();
@@ -185,18 +177,18 @@ function _reload(path) {
 }
 
 $(document).ready(function() {
-
+  
   // Workaround Firefox and IE not showing file selection dialog when clicking on "upload-file" <button>
   // Making it a <div> instead also works but then it the button doesn't work anymore with tab selection or accessibility
   $("#upload-file").click(function(event) {
     $("#fileupload").click();
   });
-
+  
   // Prevent event bubbling when using workaround above
   $("#fileupload").click(function(event) {
     event.stopPropagation();
   });
-
+  
   $("#fileupload").fileupload({
     dropZone: $(document),
     pasteZone: null,
@@ -204,19 +196,19 @@ $(document).ready(function() {
     sequentialUploads: true,
     // limitConcurrentUploads: 2,
     // forceIframeTransport: true,
-
+    
     url: 'upload',
     type: 'POST',
     dataType: 'json',
-
+    
     start: function(e) {
       $(".uploading").show();
     },
-
+    
     stop: function(e) {
       $(".uploading").hide();
     },
-
+    
     add: function(e, data) {
       var file = data.files[0];
       data.formData = {
@@ -230,54 +222,45 @@ $(document).ready(function() {
         jqXHR.abort();
       });
     },
-
+    
     progress: function(e, data) {
       var progress = parseInt(data.loaded / data.total * 100, 10);
       data.context.find(".progress-bar").css("width", progress + "%");
     },
-
+    
     done: function(e, data) {
       _reload(_path);
     },
-
+    
     fail: function(e, data) {
-
       var file = data.files[0];
-      if (data.response().jqXHR.status === 461) {
-        _showCustomizedError(data.response().jqXHR.responseJSON.message)
-        return
-      }
-      if (data.response().jqXHR.readyState !== 4) {
-        _showCustomizedError("Please make sure both devices are connected to the same Wi-Fi, and your phone screen is not locked.")
-        return
-      }
-      if (data.errorThrown !== "abort") {
-        _showError("Failed uploading \"" + file.name + "\"", data.textStatus, data.errorThrown);
+      if (data.errorThrown != "abort") {
+        _showError("Failed uploading \"" + file.name + "\" to \"" + _path + "\"", data.textStatus, data.errorThrown);
       }
     },
-
+    
     always: function(e, data) {
       data.context.remove();
     },
-
+    
   });
-
+  
   $("#create-input").keypress(function(event) {
     if (event.keyCode == ENTER_KEYCODE) {
       $("#create-confirm").click();
     };
   });
-
+  
   $("#create-modal").on("shown.bs.modal", function(event) {
     $("#create-input").focus();
     $("#create-input").select();
   });
-
+  
   $("#create-folder").click(function(event) {
     $("#create-input").val("Untitled folder");
     $("#create-modal").modal("show");
   });
-
+  
   $("#create-confirm").click(function(event) {
     $("#create-modal").modal("hide");
     var name = $("#create-input").val();
@@ -294,18 +277,18 @@ $(document).ready(function() {
       });
     }
   });
-
+  
   $("#move-input").keypress(function(event) {
     if (event.keyCode == ENTER_KEYCODE) {
       $("#move-confirm").click();
     };
   });
-
+  
   $("#move-modal").on("shown.bs.modal", function(event) {
     $("#move-input").focus();
     $("#move-input").select();
   })
-
+  
   $("#move-confirm").click(function(event) {
     $("#move-modal").modal("hide");
     var oldPath = $("#move-input").data("path");
@@ -323,11 +306,11 @@ $(document).ready(function() {
       });
     }
   });
-
+  
   $("#reload").click(function(event) {
     _reload(_path);
   });
-
+  
   _reload("/");
-
+  
 });
